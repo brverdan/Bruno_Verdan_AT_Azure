332ce515764c81e943ced03fb33b9be02e04a298
Bruno Verdan <bruno.verdan@al.infnet.edu.br>
2020-09-23 14:21:58 -0300

    Add project files.

---
+++ Domain/Amigo/Amigo.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace Domain.Amigo
+{
+    public class Amigo
+    {
+        public Guid Id { get; set; }
+        public string UrlFoto { get; set; }
+        public string Name { get; set; }
+        public string Sobrenome { get; set; }
+        public string Email { get; set; }
+        public string Telefone { get; set; }
+        public DateTime DtAniversario { get; set; }
+        public List<Amizade> Amizade { get; set; } 
+        public Pais.Pais Pais { get; set; }
+        public Estado.Estado Estado { get; set; }
+    }
+}
